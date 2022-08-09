3d6b432fb3c3550f1b2878100fcf7cc228e8fb1b
tusharraj256 <78840162+tusharraj256@users.noreply.github.com>
2022-08-09 16:24:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ const getCountry = function (country) {
     <img class="country__img" src="${data.flags.png}" />
     <div class="country__data">
       <h3 class="country__name">${data.name}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+      <h4 class="country__region">${data.region}</h4> 
+      <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} Million of  people</p>
       <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
     </div>
